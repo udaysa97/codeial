96cae09b11786eb89a6a98cd37785769dc87c7ca
udaysa97 <uday.sa97@gmail.com>
2020-07-22 00:33:46 +0530

    First commit with basic structure and express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error in server:${err}`);
+    }
+    console.log(`server running on port:${port}`);
+});
